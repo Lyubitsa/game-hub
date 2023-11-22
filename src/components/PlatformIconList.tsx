9ea0ad7ead6bc71e555e75b6f094fb578fd5fbbe
Lyubitsa G <lyubi@Lyubitsas-MacBook-Air.local>
2023-11-22 16:14:46 +0200

    Added react icons package, icon list, score of each game

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,43 @@
+import {FaWindows,
+        FaPlaystation, 
+        FaXbox, 
+        FaApple, 
+        FaLinux, 
+        FaAndroid,
+        
+    } from 'react-icons/fa'; //font awesome
+import { MdPhoneIphone} from 'react-icons/md'; // material design
+import { SiNintendo } from 'react-icons/si'; //
+import { BsGlobe } from 'react-icons/bs'; // bootstrap
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from 'react-icons/lib';
+
+interface Props {
+    platforms: Platform []
+}
+
+const PlatformIconList = ({platforms}: Props) => {
+    const iconMap: {[key: string]: IconType} = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        android: FaAndroid,
+        mac: FaApple,
+        linux: FaLinux,
+        ios: MdPhoneIphone,
+        web: BsGlobe,
+    }
+
+    return (
+
+        <HStack margin={1}>
+            {platforms.map((platform) => (
+                <Icon as= { iconMap[platform.slug]} color='gray.500'/>
+            ))}
+        </HStack>
+    )
+}
+
+export default PlatformIconList;
