2a9122da02d64e15c83727d47e5e4ae41bc41f1f
Liubi <liubi.georgieva@gmail.com>
2023-11-23 10:58:33 +0200

    new generic data hook to avoid replacing of the code

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Box, Grid, GridItem, Show } from '@chakra-ui/react'
 import NavBar from './components/NavBar'
 import GameGrid from './components/GameGrid'
 import './scss/main.scss'
+import GenreList from './components/GenreList'
 
 function App() {
 
@@ -16,7 +17,7 @@ function App() {
     </GridItem>
     <Show above="lg">
     <GridItem area="aside">
-      Aside
+      <GenreList></GenreList>
     </GridItem>
     </Show>
     
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,21 +1,21 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
-import useGames from "../hooks/useGames";
+import useGames, { Game } from "../hooks/useGames";
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
 
 
 const GameGrid = () => {
-   const {games, error, isLoading} = useGames();
+   const {data, error, isLoading} = useGames();
    const skeletons = [1, 2, 3, 4, 5, 6];
 
     return (
         <> 
         { error && <Text>{error}</Text> }
 
-        <SimpleGrid columns={{sm:1, md:2, lg:3, xl:4}} padding='10px' spacing={10}>
+        <SimpleGrid columns={{sm:1, md:2, lg:3, xl:3}} padding='10px' spacing={10}>
             {isLoading && skeletons.map(skeleton => <GameCardContainer><GameCardSkeleton  key={skeleton }/></GameCardContainer> )}
-            {games.map((game) => (
+            {data.map((game: Game) => (
               <GameCardContainer><GameCard key={game.id} game={game}/></GameCardContainer> 
             ))}
 
