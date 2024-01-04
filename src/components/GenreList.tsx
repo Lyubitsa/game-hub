a7243043b2d9686ed54c1cbf36953879150c4d13
Liubi <liubi.georgieva@gmail.com>
2024-01-04 14:32:13 +0200

    some small fixes in the layout

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,5 @@
 
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import { Button, HStack, Heading, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useData from "../hooks/useData";
 import useGenre, { Genre } from "../hooks/useGenre";
 import getGroppedImageUrl from "../services/image-url";
@@ -16,19 +16,23 @@ const GenreList = ({selectedGenre, onSelectGenere}: Props) => {
     if (isLoading) return <Spinner/>;
 
     return (
+        <>
+       <Heading  fontSize='2xl' marginBottom={3}>Genres</Heading>
         <List>
             {data.map(genre => ( 
             <ListItem key={genre.id} paddingY='5px'>
                 <HStack>
                 <Image boxSize="32px"
                        borderRadius={8}
+                       objectFit='cover'
                        src={getGroppedImageUrl(genre.image_background)}/>
-                <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenere(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+                <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenere(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
                 </HStack>
             </ListItem>
             
            ) )}
         </List>
+        </>
     )
  }
 
